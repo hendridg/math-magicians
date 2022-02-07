d5264a7920e2b8026394cfcf3c0a0b9c9b3928ce
hendridg <hendridg@gmail.com>
2022-02-07 17:49:06 -0400

    Add function btnOnClick to handle operations

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,63 +1,54 @@
 import React, { Component } from 'react';
 import './Calculator.css';
+import calculate from '../logic/calculate';
 
 export default class Calculator extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      operator1: '',
-      result: '0',
+      total: null,
+      next: null,
+      operation: null,
     };
-    this.btnOnClickOperator = this.btnOnClickOperator.bind(this);
     this.btnOnClick = this.btnOnClick.bind(this);
-    this.btnOnClickResult = this.btnOnClickResult.bind(this);
-  }
-
-  btnOnClickOperator({ target }) {
-    this.setState((prevState) => ({
-      operator1: prevState.operator1.concat(target.textContent),
-      result: prevState.operator1.concat(target.textContent),
-    }));
   }
 
   btnOnClick({ target }) {
-    this.setState(() => ({
-      operator1: target.textContent,
-    }));
-  }
-
-  btnOnClickResult() {
     this.setState((prevState) => ({
-      result: prevState.operator1 * 2,
+      total: calculate(prevState, target.textContent).total,
+      next: calculate(prevState, target.textContent).next,
+      operation: calculate(prevState, target.textContent).operation,
     }));
   }
 
   render() {
-    const { result } = this.state;
+    const { total, next, operation } = this.state;
     return (
       <div className="container">
         <div className="result">
-          {result}
+          { total || '' }
+          {operation || '' }
+          {next || ''}
         </div>
-        <button type="button" onClick={this.btnOnClickOperator} className="btn-light btn-zero">0</button>
-        <button type="button" onClick={this.btnOnClickOperator} className="btn-light btn-dot">.</button>
-        <button type="button" onClick={this.btnOnClickResult} className="btn-accent btn-equal">=</button>
+        <button type="button" onClick={this.btnOnClick} className="btn-light btn-zero">0</button>
+        <button type="button" onClick={this.btnOnClick} className="btn-light btn-dot">.</button>
+        <button type="button" onClick={this.btnOnClick} className="btn-accent btn-equal">=</button>
         <button type="button" onClick={this.btnOnClick} className="btn-light btn-clear">AC</button>
         <button type="button" onClick={this.btnOnClick} className="btn-light btn-toogle-sign">+/-</button>
         <button type="button" onClick={this.btnOnClick} className="btn-light btn-percentage">%</button>
         <button type="button" onClick={this.btnOnClick} className="btn-accent btn-divider">/</button>
-        <button type="button" onClick={this.btnOnClick} className="btn-accent btn-multiply">*</button>
+        <button type="button" onClick={this.btnOnClick} className="btn-accent btn-multiply">x</button>
         <button type="button" onClick={this.btnOnClick} className="btn-accent btn-substract">-</button>
         <button type="button" onClick={this.btnOnClick} className="btn-accent btn-plus">+</button>
-        <button type="button" onClick={this.btnOnClickOperator} className="btn-light btn-one">1</button>
-        <button type="button" onClick={this.btnOnClickOperator} className="btn-light btn-two">2</button>
-        <button type="button" onClick={this.btnOnClickOperator} className="btn-light btn-three">3</button>
-        <button type="button" onClick={this.btnOnClickOperator} className="btn-light btn-four">4</button>
-        <button type="button" onClick={this.btnOnClickOperator} className="btn-light btn-five">5</button>
-        <button type="button" onClick={this.btnOnClickOperator} className="btn-light btn-six">6</button>
-        <button type="button" onClick={this.btnOnClickOperator} className="btn-light btn-seven">7</button>
-        <button type="button" onClick={this.btnOnClickOperator} className="btn-light btn-eight">8</button>
-        <button type="button" onClick={this.btnOnClickOperator} className="btn-light btn-nine">9</button>
+        <button type="button" onClick={this.btnOnClick} className="btn-light btn-one">1</button>
+        <button type="button" onClick={this.btnOnClick} className="btn-light btn-two">2</button>
+        <button type="button" onClick={this.btnOnClick} className="btn-light btn-three">3</button>
+        <button type="button" onClick={this.btnOnClick} className="btn-light btn-four">4</button>
+        <button type="button" onClick={this.btnOnClick} className="btn-light btn-five">5</button>
+        <button type="button" onClick={this.btnOnClick} className="btn-light btn-six">6</button>
+        <button type="button" onClick={this.btnOnClick} className="btn-light btn-seven">7</button>
+        <button type="button" onClick={this.btnOnClick} className="btn-light btn-eight">8</button>
+        <button type="button" onClick={this.btnOnClick} className="btn-light btn-nine">9</button>
       </div>
     );
   }
