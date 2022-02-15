8dd7b9df66e986aea57f3506bf99b16b4c3f335a
hendridg <hendridg@gmail.com>
2022-02-15 17:17:54 -0400

    Delete comment in calculate.js file

--- src/logic/calculate.js
+++ src/logic/calculate.js
@@ -26,14 +26,12 @@ export default function calculate(obj, buttonName) {
     if (buttonName === '0' && obj.next === '0') {
       return {};
     }
-    // If there is an operation, update next
     if (obj.operation) {
       if (obj.next) {
         return { ...obj, next: obj.next + buttonName };
       }
       return { ...obj, next: buttonName };
     }
-    // If there is no operation, update next and clear the value
     if (obj.next) {
       return {
         next: obj.next + buttonName,
@@ -73,7 +71,6 @@ export default function calculate(obj, buttonName) {
         operation: null,
       };
     }
-    // '=' with no operation, nothing to do
     return {};
   }
 
@@ -86,21 +83,9 @@ export default function calculate(obj, buttonName) {
     }
     return {};
   }
-
-  // Button must be an operation
-
-  // When the user presses an operation button without having entered
-  // a number first, do nothing.
-  // if (!obj.next && !obj.total) {
-  //   return {};
-  // }
-
-  // User pressed an operation after pressing '='
   if (!obj.next && obj.total && !obj.operation) {
     return { ...obj, operation: buttonName };
   }
-
-  // User pressed an operation button and there is an existing operation
   if (obj.operation) {
     if (obj.total && !obj.next) {
       return { ...obj, operation: buttonName };
@@ -112,15 +97,9 @@ export default function calculate(obj, buttonName) {
       operation: buttonName,
     };
   }
-
-  // no operation yet, but the user typed one
-
-  // The user hasn't typed a number yet, just save the operation
   if (!obj.next) {
     return { operation: buttonName };
   }
-
-  // save the operation and shift 'next' into 'total'
   return {
     total: obj.next,
     next: null,
