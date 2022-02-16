52012c929234f1e535eb8827eb6dcade1b9f99bc
David Vergaray <david.vergaray.a@gmail.com>
2022-02-16 12:44:32 -0300

    Add components tests

---
+++ src/components/__tests__/Button.test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import Button from '../Button';
+
+it('renders correctly', () => {
+  const tree = renderer
+    .create(<Button />)
+    .toJSON();
+  expect(tree).toMatchSnapshot();
+});
